56efb17fc7bcc24edf435306c619c223cf4fcf88
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2023-02-21 13:55:55 -0600

    Remove comments from utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,25 +1,6 @@
 // This file contains the splitArr function which takes in a positive integer (groupBy) and an array (nums). The splitArr function returns the array split according to the groupBy value.
  
 
-// The challenge posed by Ram Maheshwaram:
-
-// Given an array of numbers (nums) and a user input of some integer (groupBy),
-// split the array into multiple arrays, each with a length of groupBy.
-// Exceptions: 
-  // 1. If the length of nums is less than or equal to groupBy, then return nums.
-  // 2. If the length of nums is not evenly divisible by groupBy, then the last array returned will have a length of less than groupBy.
-
-// Examples:
-// nums = [1, 2, 3, 4, 5, 6, 7, 8], groupBy = 3
-// return [[1, 2, 3], [4, 5, 6], [7, 8]]
-
-// nums = [], groupBy can be anything
-// return []
-
-// nums = [1,2], groupBy = 5
-// return [1,2]
-
-
 function splitArr(groupBy, nums=[1,2,3,4,5,6,7,8]) {
   const result = [];
   const multiples = [];
